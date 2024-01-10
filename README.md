0dbe0c4eb29bcd4807cccb6cb9c9da5670d5d79b
Rakhimbaev <Rakhimbaev@Bpcbt.com>
2024-01-10 12:30:35 +0600

    Implement first HW

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 OTUS homeworks repository
+Student Rakhimbaev Akmal
---
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -0,0 +1,9 @@
+package ru.otus;
+
+import com.google.common.base.Objects;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        System.out.println(Objects.hashCode("Hello Otus"));
+    }
+}
