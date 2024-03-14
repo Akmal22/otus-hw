d02d094069e67b0b4ba3624366822c5d750f9634
Rakhimbaev <Rakhimbaev@Bpcbt.com>
2024-03-14 14:53:12 +0500

    Fix review

--- hw10-AOP/src/main/java/ru/otus/LoggingInvocationHandler.java
+++ hw10-AOP/src/main/java/ru/otus/LoggingInvocationHandler.java
@@ -8,11 +8,11 @@ import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
 
+import static ru.otus.Utils.getMethodSignatureAsString;
+
 public class LoggingInvocationHandler implements InvocationHandler {
     private static final Logger logger = LoggerFactory.getLogger(LoggingInvocationHandler.class);
-
-    private final Set<Method> loggingMethods;
-
+    private final Set<String> loggingMethods;
     private final TestLoggingInterface proxiedObject;
 
     public LoggingInvocationHandler(TestLoggingInterface proxiedObject) {
@@ -20,18 +20,17 @@ public class LoggingInvocationHandler implements InvocationHandler {
         this.loggingMethods = new HashSet<>();
         for (Method method : proxiedObject.getClass().getMethods()) {
             if (Utils.logMethod(method)) {
-                loggingMethods.add(method);
+                loggingMethods.add(getMethodSignatureAsString(method));
             }
         }
     }
 
     @Override
-    public Object invoke(Object proxy, Method interfaceMethod, Object[] args) throws Throwable {
-        Method objectMethod = proxiedObject.getClass().getMethod(interfaceMethod.getName(), interfaceMethod.getParameterTypes());
-        if (loggingMethods.contains(objectMethod)) {
-            logger.info("executed method: {}, params: {}", interfaceMethod.getName(), args);
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (loggingMethods.contains(getMethodSignatureAsString(method))) {
+            logger.info("executed method: {}, params: {}", method.getName(), args);
         }
 
-        return interfaceMethod.invoke(proxiedObject, args);
+        return method.invoke(proxiedObject, args);
     }
 }
--- hw10-AOP/src/main/java/ru/otus/Utils.java
+++ hw10-AOP/src/main/java/ru/otus/Utils.java
@@ -1,10 +1,16 @@
 package ru.otus;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Utils {
     public static boolean logMethod(Method method) {
         return method.getDeclaredAnnotations().length > 0
                 && method.isAnnotationPresent(Log.class);
+    }
+
+    public static String getMethodSignatureAsString(Method method) {
+        return method.getName() + "::" + Arrays.stream(method.getParameterTypes()).map(Class::getName).collect(Collectors.joining("::"));
     }
 }
