8e0498ed216caa31f414d557c3b957f71455648a
Rakhimbaev <Rakhimbaev@Bpcbt.com>
2024-03-25 11:23:03 +0500

    Refactor

--- hw12-OOP/src/test/java/ru/otus/AtmTest.java
+++ hw12-OOP/src/test/java/ru/otus/AtmTest.java
@@ -14,14 +14,17 @@ public class AtmTest {
     @DisplayName("return specified amount, and subtract returned money from money in ATM")
     @Test
     void atmSuccessScenario() {
+        // Given
         Map<Ruble, Integer> initialMoney = new HashMap<>();
         initialMoney.put(new Ruble(50.0), 10);
         initialMoney.put(new Ruble(100.0), 10);
         initialMoney.put(new Ruble(500.0), 10);
         Atm<Ruble> atm = new RubleAtm(initialMoney);
 
+        // When
         Map<Ruble, Integer> cash = atm.giveOutAmount(650.0);
 
+        // Then
         assertEquals(cash.get(new Ruble(50.0)), 1);
         assertEquals(cash.get(new Ruble(100.0)), 1);
         assertEquals(cash.get(new Ruble(500.0)), 1);
@@ -53,7 +56,7 @@ public class AtmTest {
     }
 
     @Test
-    @DisplayName("throw NotProcessableAmountException if there is not enough banknotes for specified amount")
+    @DisplayName("throw NotProcessableAmountException if there is not enough money for specified amount")
     void atmNotEnoughMoney() {
         // Given
         Map<Ruble, Integer> initialMoney = new HashMap<>();
