0026767b102358d06f2130b9f2c9955ed487f7fa
Rakhimbaev <Rakhimbaev@Bpcbt.com>
2024-04-10 08:37:37 +0500

    Create MoneyStorage

--- hw12-OOP/src/main/java/ru/otus/SimpleAtm.java
+++ hw12-OOP/src/main/java/ru/otus/SimpleAtm.java
@@ -1,69 +1,33 @@
 package ru.otus;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-public class SimpleAtm implements Atm{
+public class SimpleAtm implements Atm {
     private static final Nominal MIN_NOMINAL = Nominal._50;
-
-    private final Map<Nominal, Integer> money = new HashMap<>();
+    private final MoneyStorage moneyStorage;
 
     public SimpleAtm() {
+        this.moneyStorage = new MoneyStorage();
     }
 
     public SimpleAtm(Map<Nominal, Integer> initialMoney) {
-        this.money.putAll(initialMoney);
+        moneyStorage = new MoneyStorage(initialMoney);
     }
 
     public void insert(Nominal nominal) {
-        Integer nominalAmonut = money.get(nominal);
-        if (nominalAmonut == null) {
-            money.put(nominal, 1);
-        } else {
-            money.put(nominal, ++nominalAmonut);
-        }
+        moneyStorage.insert(nominal);
     }
 
     public Map<Nominal, Integer> giveOutAmount(long amount) {
         if (amount < MIN_NOMINAL.getAmount()) {
             throw new IllegalArgumentException("Illegal amount");
         }
-        HashMap<Nominal, Integer> giveOutMoney = new HashMap<>();
-        List<Nominal> nominals = this.money.keySet().stream()
-                .sorted(Comparator.comparingLong(Nominal::getAmount))
-                .toList().reversed();
-        Map<Nominal, Integer> tmpMoney = new HashMap<>(this.money);
-        Long giveOutAmount = 0L;
-        for (Nominal nominal : nominals) {
-            int giveoutNominalsCount = 0;
-            int nominalCount = money.get(nominal);
-            while (giveOutAmount < amount && nominalCount > 0) {
-                if (giveOutAmount + nominal.getAmount() > amount) {
-                    break;
-                }
-                giveOutMoney.put(nominal, ++giveoutNominalsCount);
-                giveOutAmount += nominal.getAmount();
-                nominalCount--;
-            }
-            tmpMoney.put(nominal, nominalCount);
-        }
 
-        if (!giveOutAmount.equals(amount)) {
-            throw new NotProcessableAmountException("Inserted amount of money cannot be given out");
-        }
-
-        money.putAll(tmpMoney);
-
-        return giveOutMoney;
+        return moneyStorage.getMoney(amount);
     }
 
     @Override
     public Map<Nominal, Integer> giveOutRest() {
-        HashMap<Nominal, Integer> giveOut = new HashMap<>(this.money);
-        this.money.clear();
-
-        return giveOut;
+        return moneyStorage.getAllMoney();
     }
 }
